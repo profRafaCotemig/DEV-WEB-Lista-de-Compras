2927092d450d5c0b237fc667eb864939edca5053
profRafaCotemig <rafaelapriscila@cotemig.com>
2021-04-28 08:18:35 -0300

    Update ListaCompras.aspx.cs

--- DEV-Lista de Compras/ListaCompras.aspx.cs
+++ DEV-Lista de Compras/ListaCompras.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -28,17 +29,45 @@ namespace DEV_Lista_de_Compras
 
         protected void BtnRemoverSelecao_Click(object sender, EventArgs e)
         {
-
+            if (ListItens.SelectedIndex < 0)
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Selecione um item para remover!')", true);
+            }
+            else {
+                ListItens.Items.RemoveAt(ListItens.SelectedIndex);
+            }
         }
 
         protected void BtnRemoverTodos_Click(object sender, EventArgs e)
         {
-
+            if (ListItens.Items.Count == 0)
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Não tem itens a ser removido!')", true);
+            }
+            else {
+                ListItens.Items.Clear();
+            }
         }
 
         protected void BtnOrdenar_Click(object sender, EventArgs e)
         {
+            if (ListItens.Items.Count < 2)
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Não tem itens suficiente para ordenar!')", true);
+            }
+            else {
+                ArrayList ListBoxArray = new ArrayList();
 
+                for (int i = 0; i < ListItens.Items.Count; i++) {
+                    ListBoxArray.Add(ListItens.Items[i].Value);
+                }
+                ListItens.Items.Clear();
+
+                ListBoxArray.Sort();
+
+                ListItens.DataSource = ListBoxArray;
+                ListItens.DataBind();
+            }
         }
     }
 }
